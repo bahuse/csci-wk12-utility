8e017fa74e6424ead60b0a1c028ab8b89dc86116
Brady Huseman <bahuseman@mymail.mines.edu>
2019-11-21 09:55:08 -0700

    Finished 2nd function

--- utility.py
+++ utility.py
@@ -1,3 +1,12 @@
 
 def PrintOutput(string):
     print('OUTPUT', string)
+
+def LoadFile(name):
+    lst=[]
+    openfile=open(name, 'r')
+    readfile-openfile.readlines()
+    for line in readfile:
+        lst.append(line)
+    print('OUTPUT', lst)
+    openfile.close
