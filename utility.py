4a3e5b346acc9881d1d9084d3787f9da5195b40e
Brady Huseman <bahuseman@mymail.mines.edu>
2019-11-21 10:09:37 -0700

    Finished 3rd Function

--- utility.py
+++ utility.py
@@ -5,8 +5,13 @@ def PrintOutput(string):
 def LoadFile(name):
     lst=[]
     openfile=open(name, 'r')
-    readfile-openfile.readlines()
+    readfile=openfile.readlines()
     for line in readfile:
-        lst.append(line)
+        lines=line[:-1]
+        lst.append(lines)
     print('OUTPUT', lst)
     openfile.close
+
+def UpdateString(string, replace, index):
+    newstring=string[:index] + replace + string[(index+1):]
+    print('OUTPUT', newstring)
